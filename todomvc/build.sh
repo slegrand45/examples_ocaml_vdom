3520876ff2b9b4a4236ae7790630a504f1317aa9
Stephane Legrand <slegrand45@gmail.com>
2020-10-30 00:49:49 +0100

    Better dune

--- todomvc/build.sh
+++ todomvc/build.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-# Compile OCaml source file to OCaml bytecode
+# With dune
+#  the generated js file is _build/default/todomvc.bc.js
+dune build --profile release
+
+# Without dune
+#  the generated js file is js/todomvc.js
+#  Compile OCaml source file to OCaml bytecode
 ocamlfind ocamlc \
 	-package js_of_ocaml,js_of_ocaml-ppx,ppx_deriving,js_of_ocaml-ppx_deriving_json,js_of_ocaml.deriving,ocaml-vdom \
 	-no-check-prims -linkpkg \
 	-o todomvc.byte todomvc.ml
-
-# Build JS code from the OCaml bytecode
+#  Build JS code from the OCaml bytecode
 js_of_ocaml +gen_js_api/ojs_runtime.js --opt 3 -o js/todomvc.js todomvc.byte
